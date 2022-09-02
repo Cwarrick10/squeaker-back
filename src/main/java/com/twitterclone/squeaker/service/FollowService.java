7d9a6938255b5de0534e2debddcbb1984a0a9bfb
Nick Choi <96934316+nickchoi10@users.noreply.github.com>
2022-09-02 11:52:07 -0400

    business logic, need response body

--- src/main/java/com/twitterclone/squeaker/service/FollowService.java
+++ src/main/java/com/twitterclone/squeaker/service/FollowService.java
@@ -1,2 +1,28 @@
-package com.twitterclone.squeaker.service;public class FollowService {
+package com.twitterclone.squeaker.service;
+import com.twitterclone.squeaker.repository.FollowRepository;
+import com.twitterclone.squeaker.repository.SqueakerRepository;
+import com.twitterclone.squeaker.repository.entity.Follow;
+import com.twitterclone.squeaker.repository.entity.Squeaker;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Service
+@AllArgsConstructor(onConstructor_ = @Autowired)
+public class FollowService {
+
+    private FollowRepository followRepository;
+    private SqueakerRepository squeakerRepository;
+
+    public void addFollow(Long squeakerId){
+        Follow follow = new Follow();
+        Squeaker followed = squeakerRepository.getReferenceById(squeakerId);
+        follow.setFollowed(followed);
+        followRepository.save(follow);
+    }
 }
