7f75086e9a6be109013c918d6c35540bdd7add61
Nick Choi <96934316+nickchoi10@users.noreply.github.com>
2022-09-05 23:01:50 -0400

    rid follow

--- src/main/java/com/twitterclone/squeaker/repository/FollowRepository.java
+++
@@ -1,8 +0,0 @@
-package com.twitterclone.squeaker.repository;
-import com.twitterclone.squeaker.repository.entity.Follow;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FollowRepository extends JpaRepository<Follow, Long> {
-}
